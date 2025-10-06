2e6b660dcbf7b38394ed226901a5964a8071c0fa
kraskinaM <kraskinamaria54@gmail.com>
2025-10-06 12:39:03 +0300

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,8 @@
+number = float(input("Введите число:"))
+
+if number > 0:
+    print("Number is positive")
+elif number < 0:
+    print("Number is negative")
+else:
+ print("number is equal to zero")
---
+++ Task4.py
@@ -0,0 +1,11 @@
+num1 = float(input("Введите первое число:"))
+num2 = float(input("Введите второе число:"))
+
+if num1 == num2:
+    print("Числа равны")
+else:
+    if num1 < num2:
+        print(num1,num2)
+    else:
+        print(num2,num1)
+1
