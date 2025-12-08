68d13a72eacf6054b2785680f21a1880db915146
kraskinaM <kraskinamaria54@gmail.com>
2025-12-08 12:43:08 +0300

    Update Shop2.py

--- products/Shop2.py
+++ products/Shop2.py
@@ -2,7 +2,7 @@ products = {
     "Фрукты": [("Яблоки", 15, 60), ("Бананы", 10, 80)],
     "Овощи": [("Морковь", 20, 30)],
     "Напитки": [("Чай", 25, 50), ("Кофе", 100, 10)],
-    "Сбачки": [("Пушистые", 20, 30)]
+    "Собачки": [("Пушистые", 20, 30)]
 }
 print("Категории товаров:")
 for category, items in products.items():
@@ -26,4 +26,5 @@ for category, items in products.items():
         max_category = category
 print(f"\nКатегория с наибольшим количеством товаров: {max_category} — {max_quantity} шт.")
 total_cost = sum(item[1] * item[2] for items in products.values() for item in items)
+
 print(f"\nОбщая стоимость всех товаров в магазине: {total_cost} руб.")
